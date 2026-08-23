69ededa6baf5bfe81d77b99063b00dec3ed1c5a8
TheSmithMode <117716736+thesmithmode@users.noreply.github.com>
2026-08-23 19:20:05 +0300

    FIX: Завершить quality gate Sing-box (#416)

--- app/src/main/java/ru/ozero/app/vpn/SingboxRuntimeFingerprint.kt
+++ app/src/main/java/ru/ozero/app/vpn/SingboxRuntimeFingerprint.kt
@@ -9,7 +9,6 @@ import ru.ozero.singboxconfig.BeanSupportDecision
 import ru.ozero.singboxconfig.ConfigBuilder
 import ru.ozero.singboxconfig.PersistedProfileRecovery
 import ru.ozero.singboxconfig.RecoveryResult
-import ru.ozero.singboxfmt.KryoSerializer
 import ru.ozero.singboxroom.entity.ProxyChainStep
 import ru.ozero.singboxroom.entity.ProxyProfile
 
