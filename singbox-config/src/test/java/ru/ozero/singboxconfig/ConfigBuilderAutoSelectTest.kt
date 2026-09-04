a762646dacf46de54ced11daaf75d960ec064a6b
TheSmithMode <117716736+thesmithmode@users.noreply.github.com>
2026-09-04 08:52:33 +0300

    test(singbox): remove stale legacy block expectation

--- singbox-config/src/test/java/ru/ozero/singboxconfig/ConfigBuilderAutoSelectTest.kt
+++ singbox-config/src/test/java/ru/ozero/singboxconfig/ConfigBuilderAutoSelectTest.kt
@@ -204,7 +204,7 @@ class ConfigBuilderAutoSelectTest {
         val json = ConfigBuilder.buildSingboxAutoConfig(listOf(makeVless()))
 
         assertContains(json, "\"type\":\"direct\"")
-        assertContains(json, "\"type\":\"block\"")
+        assertFalse(json.contains("\"type\":\"block\""), "legacy block outbound was removed in sing-box 1.13")
         assertFalse(json.contains("\"type\":\"dns\""), "dns outbound removed in sing-box 1.13.0")
         assertContains(json, "\"auto_detect_interface\":true")
     }
